0f9d32668ec514e506acb12b8967c31d37e4a1b5
bjlovee <bj000051@yahoo.com>
2022-10-29 13:17:09 -0400

    setup user./new.jsx

---
+++ views/users/New.jsx
@@ -0,0 +1,22 @@
+const React = require('react')
+const Default = require('../layouts/Default.jsx')
+
+class New extends React.Component {
+  render () {
+    return (
+      <Default title='Create New Game'>
+        <nav>
+          <a href='/users'>Go Back To Comments Home Page</a>
+        </nav>
+        <form method='POST' action='/users'>
+          Name:&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='name' placeholder='Name of User Here' /><br />
+          Comment: <input type='text' name='rating' placeholder='Comment Here' /><br />
+          Like Game: <input type='checkbox' name='likeGame' /><br />
+          <input type='submit' value='Submit Comment' />
+        </form>
+      </Default>
+    )
+  }
+}
+
+module.exports = New
