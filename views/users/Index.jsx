e17a15286fcc91653c0a5203353294096b01e240
bjlovee <bj000051@yahoo.com>
2022-11-02 08:42:02 -0400

    fix dom bug/

--- views/users/Index.jsx
+++ views/users/Index.jsx
@@ -6,18 +6,18 @@ class Index extends React.Component {
     const { user, name, comment } = this.props
     return (
       <Default>
-        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous' />
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossOrigin='anonymous' />
         <link rel='stylesheet' href='/css/app.css' />
         <div className='container'>
           <h1>Welcome</h1>
 
-          {/* <div id='comment' className='card mb-2'>
+          <div id='comment' className='card mb-2'>
             <div className='card-body'>
-              <h5 className='card-title'>Name: userName</h5>
-              <h6 className='card-subtitle text-muted'>Date: 10/28/22</h6><br />
-              This is where the comment will go!
+              <h5 className='card-title'>Name: Bj Love</h5>
+              <h6 className='card-subtitle text-muted'>Date: 11/02/22</h6><br />
+              I really love this library I cant wait to see what future updates will bring!
             </div>
-          </div> */}
+          </div>
           
           
           <hr />
@@ -27,8 +27,8 @@ class Index extends React.Component {
             <form method='POST' action='/users'>
               <div id='make-comment'>
                 <div className='form-group'>
-                  <label for='name'>Name:</label>
-                  <input style={{ width: '250px' }} form-control type='text' id='name' name='name' required />
+                  <label htmlFor='name'>Name:</label>
+                  <input style={{ width: '250px' }} form-control="true" type='text' id='name' name='name' required />
                 </div>
                 <div className='form-group'>
                   <label for='message'>Type Comment Below!</label>
